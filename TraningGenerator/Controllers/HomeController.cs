e851c51404592b61db0bddf50164b3b66fe1711b
Emmy Lindgren <id19eln@cs.umu.se>
2021-11-04 18:55:15 +0100

    Intial commit. One View created.

---
+++ TraningGenerator/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TraningGenerator.Models;
+
+namespace TraningGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TrainingRecommendation(IFormCollection col)
+        {
+            PersonTrainingInfo pti = new PersonTrainingInfo();
+            pti.Name = col["Name"];
+            pti.YearOfBirth = Convert.ToInt32(col["YearOfBirth"]);
+
+            pti.TrainingNow = Convert.ToInt32(col["TrainingNow"]);
+            pti.FavoriteTraining = col["FavoriteTraining"];
+
+            //Lägg till när vi har fått till beräkningen 
+            //pti.CalculateNewTraining();
+
+            string s = JsonConvert.SerializeObject(pti);
+            HttpContext.Session.SetString("")
+
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TraningGenerator/Models/PersonTrainingInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraningGenerator.Models
+{
+    public class PersonTrainingInfo
+    {
+        [Required]
+        [Display (Name ="Namn")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Födelseår")]
+        public int YearOfBirth { get; set; }
+
+        public int Age { get; set; }
+
+        [Required]
+        [Display(Name = "Hur mycket tränar du idag?")]
+        public int TrainingNow { get; set; }
+
+        public int NewTraining { get; set; }
+
+        [Required]
+        [Display(Name = "Vad är din favoritträning?")]
+        public string FavoriteTraining { get; set; }
+
+        public PersonTrainingInfo()
+        {
+            //lägg till konstruktor här. 
+        }
+
+        public void CalculateNewTraining()
+        {
+            Age = YearOfBirth - DateTime.Now.Year;
+            //lägg till beräkning av ny träning här också! 
+
+        }
+    }
+}
